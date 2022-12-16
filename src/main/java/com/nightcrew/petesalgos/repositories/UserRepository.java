dae3b14fe30049dcc68b86cc9bcd8de4eb789a71
Jacob Reynolds <jacobreynolds2012@gmail.com>
2022-12-15 16:44:50 -0800

    First Commit

--- src/main/java/com/nightcrew/petesalgos/repositories/UserRepository.java
+++ src/main/java/com/nightcrew/petesalgos/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.nightcrew.petesalgos.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository {
+import com.nightcrew.petesalgos.models.User;
 
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>  {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/nightcrew/petesalgos/services/UserService.java
+++ src/main/java/com/nightcrew/petesalgos/services/UserService.java
@@ -1,8 +1,72 @@
 package com.nightcrew.petesalgos.services;
 
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.nightcrew.petesalgos.models.LoginUser;
+import com.nightcrew.petesalgos.models.User;
+import com.nightcrew.petesalgos.repositories.UserRepository;
+
 
 @Service
 public class UserService {
+
+    @Autowired
+	private UserRepository userRepo;
+	
+	public User register(User newUser, BindingResult result) {
+		// TO-DO - Reject values: 
+        // Reject if email is taken (present in database)
+        // 1. Find user in the DB by email    
+	   Optional<User> optionalUser = userRepo.findByEmail(newUser.getEmail());
+	   
+        // 2. if the email is present , reject
+	   if(optionalUser.isPresent()) {
+		   result.rejectValue("email", "unique", "This email is already registered!");
+	   }
+        // Reject if password doesn't match confirmation
+	   if(!newUser.getPassword().equals(newUser.getConfirm())) {
+		   result.rejectValue("password", "matches", "This confirm password does not match!");
+	   }
+	   
+        // if result has errors, return 
+	   if(result.hasErrors()) {
+		   return null;
+	   }
+	   
+        // Hash and set password, save user to database 
+	   String hashed = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+	   newUser.setPassword(hashed);
+	   return userRepo.save(newUser);
+	   
+	}
+	
+	public User login(LoginUser newLogin, BindingResult result) {
+		Optional<User> optionalUser = userRepo.findByEmail(newLogin.getEmail());
+		// 2. if the email is present , reject
+		 if(!optionalUser.isPresent()) {
+			 result.rejectValue("email", "unique", "This email is already registered!");
+			 return null;
+	}
+		 // 3.1 grab the user from potential user   
+			User user = optionalUser.get();
+			
+	        // 3.2 if BCrypt password match fails
+			if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+			    result.rejectValue("password", "Matches", "Invalid Password!");
+			}
+
+	        // 4 if result has errors,return
+			if(result.hasErrors()) {
+				   return null;
+			   }
+			// Otherwise, return the user object
+	    	return user;
+		
+        } 
 
 }
