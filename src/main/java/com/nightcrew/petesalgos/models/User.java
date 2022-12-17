0a3fa0f2db8e88629a0b1b246a891ee73a9fb692
Bryan Park <bryan-j-park@outlook.com>
2022-12-16 14:34:30 -1000

    updated database

--- src/main/java/com/nightcrew/petesalgos/models/User.java
+++ src/main/java/com/nightcrew/petesalgos/models/User.java
@@ -11,6 +11,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
@@ -26,50 +29,64 @@ import org.springframework.format.annotation.DateTimeFormat;
 	@Table(name="users")
 	public class User {
 		@Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
 		
 		@NotEmpty(message="Username is required!")
-	    @Size(min=3, max=30, message="Username must be between 3 and 30 characters")
-	    private String userName;
-	    
-	    @NotEmpty(message="Email is required!")
-	    @Email(message="Please enter a valid email!")
-	    private String email;
-	    
-	    @NotEmpty(message="Password is required!")
-	    @Size(min=8, max=128, message="Password must be between 8 and 128 characters")
-	    private String password;
-	    
-	    @Transient
-	    @NotEmpty(message="Confirm Password is required!")
-	    @Size(min=8, max=128, message="Confirm Password must be between 8 and 128 characters")
-	    private String confirm;
-	    
-	    @Column(updatable=false)
-	    @DateTimeFormat(pattern="yyy-MM-dd")
-	    private Date createdAt;
-	    
-	    @DateTimeFormat(pattern="yyyy-MM-dd")
-	    private Date updatedAt;
-	    
-	    @PrePersist
-	    protected void onCreated() {
-	    	this.createdAt = new Date();
-	    }
-	    	
-	    @PreUpdate
-	    protected void onUpdate() {
-	    	this.updatedAt = new Date();
-	    }
+		@Size(min=3, max=30, message="Username must be between 3 and 30 characters")
+		private String userName;
+		
+		@NotEmpty(message="Email is required!")
+		@Email(message="Please enter a valid email!")
+		private String email;
+		
+		@NotEmpty(message="Password is required!")
+		@Size(min=8, max=128, message="Password must be between 8 and 128 characters")
+		private String password;
+		
+		@Transient
+		@NotEmpty(message="Confirm Password is required!")
+		@Size(min=8, max=128, message="Confirm Password must be between 8 and 128 characters")
+		private String confirm;
+		
+		@Column(updatable=false)
+		@DateTimeFormat(pattern="yyy-MM-dd")
+		private Date createdAt;
+		
+		@DateTimeFormat(pattern="yyyy-MM-dd")
+		private Date updatedAt;
+		
+		@PrePersist
+		protected void onCreated() {
+			this.createdAt = new Date();
+		}
+			
+		@PreUpdate
+		protected void onUpdate() {
+			this.updatedAt = new Date();
+		}
       //joining person with License as a 1:n
-			@OneToMany(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
-			private List<Problem> problem;
+		@OneToMany(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+		private List<Problem> problem;
+		
+		@ManyToMany(fetch = FetchType.LAZY)
+		@JoinTable(
+			name = "favorites",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "problem_id")
+		)
+		private List<Problem> favorites;
+
+		@ManyToMany(fetch = FetchType.LAZY)
+		@JoinTable(
+			name = "comments",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "problem_id")
+		)
+		private List<Problem> problems;
 
 
-
-
-	    public User() {}
+		public User() {}
 
 		public Long getId() {
 			return id;
